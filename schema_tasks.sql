33d348cc9415965f11108ed01d607f690479a1c0
lafont-loic <loic.lafont.isr@gmail.com>
2026-02-18 15:54:38 +0100

    Update schema_tasks.sql

--- schema_tasks.sql
+++ schema_tasks.sql
@@ -1,1 +1,9 @@
+DROP TABLE IF EXISTS tasks;
 
+CREATE TABLE tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titre TEXT NOT NULL,
+    description TEXT,
+    date_echeance TEXT,
+    termine INTEGER DEFAULT 0 -- 0 = En cours, 1 = Terminé
+);
